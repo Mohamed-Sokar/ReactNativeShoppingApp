2a351c824cff40499f31e17e7c751f34f828c4bb
Mohamed-Sokar <109233779+Mohamed-Sokar@users.noreply.github.com>
2022-12-19 23:15:43 +0200

    added icons to use them ibottom-added icon to use them in bottom tab navigation

---
+++ src/assests/Icons/Navigation/index.js
@@ -0,0 +1,23 @@
+import HomeOff from './HomeOff';
+import HomeOn from './HomeOn';
+import CartOff from './CartOff';
+import CartOn from './CartOn';
+import ProfileOff from './ProfileOff';
+import ProfileOn from './ProfileOn';
+import ProductsOff from './ProductsOff';
+import ProductsOn from './ProductsOn';
+import CatagoriesOn from './CatagoriesOn';
+import CatagoriesOff from './CatagoriesOff';
+
+export {
+  HomeOff,
+  HomeOn,
+  CartOff,
+  CartOn,
+  ProfileOff,
+  ProfileOn,
+  ProductsOff,
+  ProductsOn,
+  CatagoriesOn,
+  CatagoriesOff,
+};
